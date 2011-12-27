9d521ecbcaab2022b2a444859fbfb5fdf55e4325
Damfly <damienhoulle@gmail.com>
2011-12-27 11:21:14 +0100

    Ajout de useful methodes

---
+++ Courses/week1/usefulmethods/localvariable.rb
@@ -0,0 +1,10 @@
+a = "Coucou"
+
+def mtd
+  b = "Coucou Damien"
+  a = "coucou Caroline"
+  puts local_variables
+end
+
+mtd
+puts local_variables
---
+++ Courses/week1/usefulmethods/printf.rb
@@ -0,0 +1,1 @@
+printf("Number: %5.2f,\nString: %s\n", 7.18, "Coucou Damien")
