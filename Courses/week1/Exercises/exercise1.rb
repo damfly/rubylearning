89e4ccbf9082aa00fc15c12b1bf081f6d4a50e34
Damfly <damienhoulle@gmail.com>
2011-12-27 11:25:02 +0100

    Add learnings thoughts

--- Courses/week1/Exercises/exercise1.rb
+++ Courses/week1/Exercises/exercise1.rb
@@ -1,8 +1,21 @@
+=begin
+  * Name: exercise1.rb
+  * Description : Exercise about precedence
+  * Author: Damien HOULLE
+  * Date: 25/12/2012
+=end
+
 y = false
 z = true
-x = y or z 
-puts x #=>true FAUX le résultat est FALSE car le "=" est prévalent sur le "or"
+x = y or z  
+puts x # => FALSE
+x = y || z
+puts x # => TRUE
 (x = y) or z 
-puts x #=>false OK
+puts x # => FALSE
 x = (y or z)
-puts x #=>true OK
+puts x # => TRUE
+
+# Learning : brackets have higher precedence over the assignement operators
+# Learning 2 : Boolean operator, || has a higher precedence over the assignement operators (=)
+# Learning 3 : "or" and "and" operators have lower precedence over the assignement operators (=)
