5878c6d2343a0c338cf09e744a7a510cd4ceb4a1
Damfly <damienhoulle@gmail.com>
2011-12-27 11:46:27 +0100

    Adding the % operator discussion

--- Courses/week1/usefulmethods/printf.rb
+++ Courses/week1/usefulmethods/printf.rb
@@ -1,1 +1,11 @@
 printf("Number: %5.2f,\nString: %s\n", 7.18, "Coucou Damien")
+
+# % is an operator
+# %[flag][width][.precision]type defines the format to be applied to the element after the %
+#in the example below we have :
+#in %05d, 0 is the flag : Spaces should be padded with 0 and not spaces
+#         5 is the width: we want a string of 5 characters maximum
+#         d is the type : it converts the argument (123 here) into a decimal number
+#
+puts "%05d" % 123 #=> Prints out 00123
+
