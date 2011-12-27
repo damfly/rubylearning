a95d7955abbf6e79df32959e72f79a518faa7edf
Damfly <damienhoulle@gmail.com>
2011-12-27 14:04:19 +0100

    Adding ages_to_years_and_months challenge solution

--- Courses/week1/Challenges/ages_to_years_and_months.rb
+++ Courses/week1/Challenges/ages_to_years_and_months.rb
@@ -1,34 +1,48 @@
-#ages_to_years_and_months.rb
+=begin
+  * Name: ages_to_years_and_months.rb 
+  * Description : use formatting
+  * Author: Damien HOULLE
+  * Date: 27/12/2012
+=end
+
+#Assignement
 #Victor Goff's challenge
-#working out the age in years and months form
-#
+#Working out the age in years and months form
+
 #Lists of age_in_seconds to transform
-#
+
 #979000000
 #2158493738
 #246144023
 #1270166272
 #1025600095
-#
-#4 years (leap years) = 1461 days
-#
-ages_in_seconds = %w{979000000 2158493738 246144023 1270166272 1025600095}
 
 
-def seconds_to_years(seconds)
-  years = 4.to_f * seconds.to_f / (3600.to_f * 24.to_f) / 1461.to_f
-  months = (years - years.to_i) * 12
+
+#Definition of constants
+SECONDS_IN_MINUTE = 60
+MINUTES_IN_HOUR = 60
+HOURS_IN_DAY = 24
+DAYS_IN_YEAR = 365.25
+
+def age_in_seconds_to_years_and_months(seconds)
+  seconds_in_day = HOURS_IN_DAY  * MINUTES_IN_HOUR * SECONDS_IN_MINUTE
+  seconds_in_year = seconds_in_day * DAYS_IN_YEAR 
+  age_in_years = seconds / seconds_in_year
+  months = (age_in_years - age_in_years.to_i) * 12
+
   if months.to_i != 0
     if months.to_i == 1
-    "#{years.to_i} years and 1 month"
+    "#{age_in_years.to_i} years and 1 month"
     end
-    "#{years.to_i} years and #{months.to_i} months"
+    "#{age_in_years.to_i} years and #{months.to_i} months"
   else
-    "#{years.to_i} years"
+    "#{age_in_years.to_i} years"
   end
-
 end
 
+ages_in_seconds = [979000000, 2158493738, 246144023, 1270166272, 1025600095]
+
 ages_in_seconds.each do |seconds|
-  puts "#{seconds} seconds => #{seconds_to_years(seconds)}"
+  puts "#{seconds} seconds => #{age_in_seconds_to_years_and_months(seconds)}"
 end
